f72c5fd5ec0a5dbbceb4cb2315c04b6a45ead2df
juangallostra <juangallostra@gmail.com>
2018-04-18 23:56:00 +0200

    Update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 setup(
   name='colourlovers',
   packages=['colourlovers'], # this must be the same as the name above
-  version='0.1',
+  version='0.1.1',
   description='A python wrapper for ColourLovers API',
   long_descritpion=open("README.rst").read() + "\n" +
   open("CHANGES.rst").read() + "\n",
@@ -10,7 +10,7 @@ setup(
   author_email='juangallostra@gmail.com',
   license='MIT',
   url='https://github.com/juangallostra/Colourlovers-API-wrapper', # use the URL to the github repo
-  download_url='https://github.com/juangallostra/Colourlovers-API-wrapper/archive/0.1.tar.gz', # I'll explain this in a second
+  download_url='https://github.com/juangallostra/Colourlovers-API-wrapper/archive/0.1.1.tar.gz', # I'll explain this in a second
   keywords=['color', 'colour', 'palette', 'api', 'colourlovers', 'wrapper'],
   classifiers=[
       'Development Status :: 3 - Alpha',
